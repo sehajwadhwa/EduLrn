534948f8df8979b8d713e68adf6fdee22d1ea3be
Sehaj Wadhwa <104042287+sehajwadhwa@users.noreply.github.com>
2023-10-18 09:12:51 -0400

    Co-authored-by: sanjivanisngh@gmail.com <sanjivanisngh@gmail.com>

--- edulrn_client/src/components/InstructorProfile/InstructorProfile.jsx
+++ edulrn_client/src/components/InstructorProfile/InstructorProfile.jsx
@@ -2,9 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./InstructorProfile.scss";
 import HamburgerMenu from "../HamburgerMenu/HamburgerMenu";
+import {useState, useEffect} from "react";
 
 
-export default function InstructorProfile(user,setUser ) {
+export default function InstructorProfile(coursesData, user, setUser ) {
+  console.log(coursesData);
+  const [instructorCourses, setInstructorCourses] = useState();
+
+  useEffect(() => {
+    const filteredData = coursesData.filter(course => course.name === user.displayName);
+    setInstructorCourses(filteredData);
+  })
+  
   return (
 
     <section className="Instructor">
@@ -38,9 +47,20 @@ export default function InstructorProfile(user,setUser ) {
       </div>
       
     <div className="Instructor__courses">
-    <Link to="/courses">
         <p className="Instructor__courses--text">My Courses</p>
-      </Link>
+        <div>
+           {
+            instructorCourses.map(course => (
+              <div>
+                <img src={`http://localhost:5000${course.img}`} alt="abc"></img>
+                <div class="Courses__display--right">
+                  <h3>{course.name}</h3>
+                  <p>{course.course_duration}</p>
+                </div>
+              </div>
+            ))
+           }
+      </div>
       
       <Link to="/courses">
         <p className="Instructor__courses--text">All Courses</p>
